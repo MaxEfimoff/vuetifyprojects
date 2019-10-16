1754c99784ce099e267fbbef432a53f73e085883
Max Efimoff <emv3@ya.ru>
2019-10-16 12:42:26 +0400

    created projects and team pajes

---
+++ src/views/Projects.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="projects">
+    <h1>projects</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="team">
+    <h1>team</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
