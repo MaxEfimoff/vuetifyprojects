bb503ca79ba99a87de7581c79affbf53754621c8
Max Efimoff <emv3@ya.ru>
2019-10-17 10:28:06 +0400

    menu updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,21 @@
         <span>Projects</span>
       </v-toolbar-title>
       <v-spacer/>
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" color="grey">
+            <v-icon left>mdi-chevron-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>mdi-logout</v-icon>
